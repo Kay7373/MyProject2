8f8fddda6fdf9c42dd6e26383c3346a16faea3ef
Andrey Koshelev <k-a-y73@yandex.ru>
2023-01-15 17:21:07 +0300

    new

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -4,13 +4,13 @@ public class Exercise1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое число: ");
-        int num1 = sc.nextInt();
+        double num1 = sc.nextDouble();
 
         System.out.println("Введите второе число: ");
-        int num2 = sc.nextInt();
+        double num2 = sc.nextDouble();
 
         if (Math.abs(num1) > Math.abs(num2)) {
-            int rez = num1 / 2;
+            double rez = num1 / 2;
             System.out.println("Первое число деленное на два: "+rez);
         }
         else {
